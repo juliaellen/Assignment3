be9560e1ec39c78bf83967ec7e9136b2f8637356
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-11-05 19:40:38 +0000

    Added github token

--- SoftwareEngineeringAss3.R
+++ SoftwareEngineeringAss3.R
@@ -12,4 +12,5 @@ myapp <- oauth_app(appname = "Assignment3",
                    key = "8a67183dbf7c715328dc",
                    secret = "b9519bed5efe29c94ca84ec5ff202eec475e029f")
 
-
+github_token1 <- oauth2.0_token(oauth_endpoints("github"), GithubApp)
+myToken <- config(token = github_token1)
