3f0a91b210524fecde51950cfa9dffd676250538
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-11-05 19:49:32 +0000

    Saved all users to be analysed on a list

--- SoftwareEngineeringAss3.R
+++ SoftwareEngineeringAss3.R
@@ -18,3 +18,14 @@ myToken <- config(token = github_token1)
 data1 <- fromJSON("https://api.github.com/users/juliaellen/followers")
 id = data1$login
 UserIds = c(id) 
+
+AllUsers = c()
+for (i in 1:length(UserIds))
+{
+  data2 = fromJSON(paste0("https://api.github.com/users/", UserIds[i], "/followers"))
+  id2 = data2$login
+  for (j in 1:length(id2))
+  {
+    UserIds[length(UserIds) +1] = id2[j]
+  }
+}
