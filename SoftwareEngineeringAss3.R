ced46ee29a1142a7fba53acc1a2fe197ff05e764
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-11-06 10:00:51 +0000

    edited followers and following

--- SoftwareEngineeringAss3.R
+++ SoftwareEngineeringAss3.R
@@ -1,29 +1,26 @@
 id = followersDF1$login
 UserIds = c(id)
 AllUsers = c()
-AllUsersDF = data.frame(Username = integer())
 AllUsersDF = data.frame(Username = integer(), Following = integer(), Followers = integer())
+AllUsersDF = data.frame(Username = integer(), Followers = integer(), Following = integer())
 for (i in 1:length(UserIds))
-  
-data2 = GET(url2, myToken)
 content2 = content(data2)
 followersDF2 = jsonlite::fromJSON(jsonlite::toJSON(content2))
-id2 = followersDF2$login
-for (j in 1:length(id2))
-  usernames = followersDF2$login
+usernames = followersDF2$login
+
 followers = followersDF2$followers
 following = followersDF2$following
 for (j in 1:length(usernames))
 {
-  if(is.element(id2[j], AllUsers) == FALSE)
-    if(is.element(usernames[j], AllUsers) == FALSE)
-    {
-      AllUsers[length(AllUsers) +1] = id2[j]
-      AllUsersDF[nrow(AllUsersDF)+1,] = c(id2[j])
-      AllUsers[length(AllUsers) +1] = usernames[j]
-      AllUsersDF[nrow(AllUsersDF)+1,] = c(usernames[j], followers, following)
-      
-    }
-  next
+  if(is.element(usernames[j], AllUsers) == FALSE)
+  {
+    url3 = paste("https://api.github.com/users/", usernames[j], sep = "")
+    data3 = GET(url3, myToken)
+    content3 = content(data3)
+    followersDF3 = jsonlite::fromJSON(jsonlite::toJSON(content3))
+    followers = followersDF3$followers
+    following = followersDF3$following
+    AllUsers[length(AllUsers) +1] = usernames[j]
+    AllUsersDF[nrow(AllUsersDF)+1,] = c(usernames[j], followers, following)
 
 
