4481920d4068922d3f99d54a2f080712191551a6
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-11-06 09:59:26 +0000

    added followers and following to a data frame

--- SoftwareEngineeringAss3.R
+++ SoftwareEngineeringAss3.R
@@ -2,19 +2,28 @@ id = followersDF1$login
 UserIds = c(id)
 AllUsers = c()
 AllUsersDF = data.frame(Username = integer())
+AllUsersDF = data.frame(Username = integer(), Following = integer(), Followers = integer())
 for (i in 1:length(UserIds))
+  
+data2 = GET(url2, myToken)
+content2 = content(data2)
+followersDF2 = jsonlite::fromJSON(jsonlite::toJSON(content2))
+id2 = followersDF2$login
+for (j in 1:length(id2))
+  usernames = followersDF2$login
+followers = followersDF2$followers
+following = followersDF2$following
+for (j in 1:length(usernames))
 {
-  url2 = paste("https://api.github.com/users/", UserIds[i], "/followers", sep = "")
-
   if(is.element(id2[j], AllUsers) == FALSE)
-  {
-    AllUsers[length(AllUsers) +1] = id2[j]
-    AllUsersDF[nrow(AllUsersDF)+1,] = c(id2[j])
-    
-  }
+    if(is.element(usernames[j], AllUsers) == FALSE)
+    {
+      AllUsers[length(AllUsers) +1] = id2[j]
+      AllUsersDF[nrow(AllUsersDF)+1,] = c(id2[j])
+      AllUsers[length(AllUsers) +1] = usernames[j]
+      AllUsersDF[nrow(AllUsersDF)+1,] = c(usernames[j], followers, following)
+      
+    }
   next
-}
-next
-
 
 
