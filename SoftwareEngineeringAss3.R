f1d68ce2c4f7e00129540c939359f64e6799f593
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-11-06 10:01:47 +0000

    Added number of repositories to data frame

--- SoftwareEngineeringAss3.R
+++ SoftwareEngineeringAss3.R
@@ -1,26 +1,19 @@
 id = followersDF1$login
 UserIds = c(id)
 AllUsers = c()
-AllUsersDF = data.frame(Username = integer(), Following = integer(), Followers = integer())
 AllUsersDF = data.frame(Username = integer(), Followers = integer(), Following = integer())
+AllUsersDF = data.frame(Username = integer(), Followers = integer(), Following = integer(), NumberOfRepositories = integer())
 for (i in 1:length(UserIds))
-content2 = content(data2)
-followersDF2 = jsonlite::fromJSON(jsonlite::toJSON(content2))
-usernames = followersDF2$login
+  
+followersDF3 = jsonlite::fromJSON(jsonlite::toJSON(content3))
+followers = followersDF3$followers
+following = followersDF3$following
+reposnumber = followersDF3$public_repos
+AllUsers[length(AllUsers) +1] = usernames[j]
+AllUsersDF[nrow(AllUsersDF)+1,] = c(usernames[j], followers, following)
+AllUsersDF[nrow(AllUsersDF)+1,] = c(usernames[j], followers, following, reposnumber)
 
-followers = followersDF2$followers
-following = followersDF2$following
-for (j in 1:length(usernames))
-{
-  if(is.element(usernames[j], AllUsers) == FALSE)
-  {
-    url3 = paste("https://api.github.com/users/", usernames[j], sep = "")
-    data3 = GET(url3, myToken)
-    content3 = content(data3)
-    followersDF3 = jsonlite::fromJSON(jsonlite::toJSON(content3))
-    followers = followersDF3$followers
-    following = followersDF3$following
-    AllUsers[length(AllUsers) +1] = usernames[j]
-    AllUsersDF[nrow(AllUsersDF)+1,] = c(usernames[j], followers, following)
+}
+next
 
 
